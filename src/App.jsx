ed45708f106e775acfb91057b1579fd6102d5465
André Tavares <andretavares3@gmail.com>
2024-05-22 17:58:16 -0300

    fundamentals of components and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from "./Post"
+import { Header } from "./components/Header"
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles.css"
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Post author="André" content="teste de conteudo" />
+      <Post author={"André Tavares"} content={"teste de conteudo 2"} />
+      <Post />
     </>
   )
 }
-
-export default App
