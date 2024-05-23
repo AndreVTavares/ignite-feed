e46074b47bd855ffb0ea5ade186aa8d48608d4f4
André Tavares <andretavares3@gmail.com>
2024-05-23 10:51:20 -0300

    creating sidebar and header components and modules css for both

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css"
 
 import "./global.css"
 
@@ -7,9 +10,14 @@ export function App() {
   return (
     <>
       <Header />
-      <Post author="André" content="teste de conteudo" />
-      <Post author={"André Tavares"} content={"teste de conteudo 2"} />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="André" content="teste de conteudo" />
+          <Post author={"André Tavares"} content={"teste de conteudo 2"} />
+          <Post />
+        </main>
+      </div>
     </>
   )
 }
